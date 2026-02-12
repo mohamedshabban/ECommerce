4d6716bcf671c72627409676ca328ca7c41d168d
Mohamed Shaban <57798592+mohamedshabban@users.noreply.github.com>
2026-02-12 11:09:23 +0200

    add pending files

--- src/frontend/ecommerce-web/src/environments/environment.ts
+++ src/frontend/ecommerce-web/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5001/api',
+  apiUrl: 'https://localhost:5171/api',
   paypal: {
     clientId: 'your-paypal-sandbox-client-id'
   },
